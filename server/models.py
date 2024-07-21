94ace0ddc8acec567a7e05b6a092aa5391a2a619
wambui11nderitu <wambuinderitu93@gmail.com>
2024-07-21 12:15:13 +0300

    updating labs

--- server/models.py
+++ server/models.py
@@ -2,44 +2,55 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.ext.associationproxy import association_proxy
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
 
 class Employee(db.Model):
-    __tablename__ = 'employees'
+    __tablename__ = "employees"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    reviews = db.relationship("Review", back_populates="employee")
+    onboarding = db.relationship('Onboarding', uselist=False, back_populates='employee')
     def __repr__(self):
-        return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
+        return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
 
 
-class Meeting(db.Model):
-    __tablename__ = 'meetings'
+class Onboarding(db.Model):
+    __tablename__ = "onboardings"
 
     id = db.Column(db.Integer, primary_key=True)
-    topic = db.Column(db.String)
-    scheduled_time = db.Column(db.DateTime)
-    location = db.Column(db.String)
+    orientation = db.Column(db.DateTime)
+    forms_complete = db.Column(db.Boolean, default=False)
+    
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
 
+    employee = db.relationship('Employee', back_populates='onboarding')
     def __repr__(self):
-        return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
+        return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
 
-class Project(db.Model):
-    __tablename__ = 'projects'
+class Review(db.Model):
+    __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    budget = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+    summary = db.Column(db.String)
+
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping the review to related employee
+    employee = db.relationship('Employee', back_populates="reviews")
 
     def __repr__(self):
-        return f'<Review {self.id}, {self.title}, {self.budget}>'
+        return f'<Review {self.id}, {self.year}, {self.summary}>'
--- server/seed.py
+++ server/seed.py
@@ -3,41 +3,42 @@
 
 import datetime
 from app import app
-from models import db, Employee, Meeting, Project
+from models import db, Employee, Review, Onboarding
 
 with app.app_context():
-
     # Delete all rows in tables
     Employee.query.delete()
-    Meeting.query.delete()
-    Project.query.delete()
+    Review.query.delete()
+    Onboarding.query.delete()
 
-    # Add employees
-    e1 = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
-    e2 = Employee(name="Tristan Tal", hire_date=datetime.datetime(2020, 1, 30))
-    e3 = Employee(name="Sasha Hao", hire_date=datetime.datetime(2021, 12, 1))
-    e4 = Employee(name="Taylor Jai", hire_date=datetime.datetime(2015, 1, 2))
-    db.session.add_all([e1, e2, e3, e4])
+    # Add model instances to database
+    uri = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
+    tristan = Employee(name="Tristan Tal", hire_date=datetime.datetime(2020, 1, 30))
+    db.session.add_all([uri, tristan])
     db.session.commit()
 
-    # Add meetings
-    m1 = Meeting(topic="Software Engineering Weekly Update",
-                 scheduled_time=datetime.datetime(
-                     2023, 10, 31, 9, 30),
-                 location="Building A, Room 142")
-    m2 = Meeting(topic="Github Issues Brainstorming",
-                 scheduled_time=datetime.datetime(
-                     2023, 12, 1, 15, 15),
-                 location="Building D, Room 430")
-    db.session.add_all([m1, m2])
+    # 1..many relationship between Employee and Review
+    uri_2023 = Review(year=2023, summary="Great web developer!",
+                      employee=uri)
+    tristan_2021 = Review(year=2021, summary="Good coding skills, often late to work",
+                          employee=tristan)
+    tristan_2022 = Review(year=2022, summary="Strong coding skills, takes long lunches"
+                          , employee=tristan)
+    tristan_2023 = Review(year=2023, summary="Awesome coding skills, dedicated worker",
+                          employee=tristan)
+    db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
     db.session.commit()
 
-    # Add projects
-    p1 = Project(title="XYZ Project Flask server",  budget=50000)
-    p2 = Project(title="XYZ Project React UI", budget=100000)
-    db.session.add_all([p1, p2])
-    db.session.commit()
+    # 1..1 relationship between Employee and Onboarding
+    uri_onboarding = Onboarding(
+    orientation=datetime.datetime(2023, 3, 27),
+    employee=uri)
 
-    # Many-to-many relationship between employee and meeting
+tristan_onboarding = Onboarding(
+    orientation=datetime.datetime(2020, 1, 20, 14, 30),
+    forms_complete=True,
+    employee=tristan)
 
-    # Many-to-many relationship between employee and project through assignment
+db.session.add_all([uri_onboarding, tristan_onboarding])
+
+db.session.commit()
